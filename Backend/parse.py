5e56c21d21b5e3db25cf7e215fb74961ff745df8
Shamanthak Hegde <shamanthak.hegde@gmail.com>
2025-03-11 20:04:40 +0000

    modified_parsing

--- Backend/parse.py
+++ Backend/parse.py
@@ -1,4 +1,4 @@
-import textract
+import textract, validators
 from youtube_transcript_api import YouTubeTranscriptApi
 from urllib.parse import urlparse, parse_qs
 from bs4 import BeautifulSoup
@@ -9,7 +9,7 @@ random.seed(42)
 def parse_text_file(file_path):
     with open(file_path, "r") as file:
         text = file.read()
-        return {"link": file_path, "type": "plaintext", "content": text}
+    return {"link": file_path, "type": "plaintext", "content": text}
     
 def parse_docx_file(file_path):
     text = textract.process(file_path)
@@ -39,11 +39,7 @@ def parse_web_content(response):
 
 
 def check_link_content(link):
-    """Determines whether the link is a PDF or plaintext and extracts content if plaintext."""
-    
-    if not link:
-        return {"link": "No link found.",
-                "type": "Error: No link found"}
+    """Determines whether the link is a PDF, Doc, Docx, Youtube or plaintext and extracts content if plaintext."""
     
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36"}
     i = 0
@@ -62,44 +58,73 @@ def check_link_content(link):
         return youtube_transcript(parsed_url)
 
     if "application/pdf" in content_type:
-        with open('./file.pdf', "wb") as file:
+        file_name = f'file_{random.randint(0, 1000)}.pdf'
+        with open(file_name, "wb") as file:
             for chunk in response.iter_content(chunk_size=8192):
                 file.write(chunk)
         print(f"✅ PDF downloaded successfully: {'./'}")
-        return parse_pdf_file('./file.pdf')
+        out = parse_pdf_file(file_name)
+        if os.path.exists(file_name):
+            os.remove(file_name)
+        return out
     
     elif "text/plain" in content_type:
-        return parse_text_file(response)
+        file_name = f'file_{random.randint(0, 1000)}.txt'
+        with open(file_name, "wb") as file:
+            for chunk in response.iter_content(chunk_size=8192):
+                file.write(chunk)
+        print(f"✅ Txt downloaded successfully: {'./'}")
+        out = parse_text_file(file_name)
+        if os.path.exists(file_name):
+            os.remove(file_name)
+        return out
     
     elif "text/html" in content_type:
         return parse_web_content(response)
     
     elif "application/msword" in content_type:
-        with open('./file.doc', "wb") as file:
+        file_name = f'file_{random.randint(0, 1000)}.doc'
+        with open(file_name, "wb") as file:
             for chunk in response.iter_content(chunk_size=8192):
                 file.write(chunk)
         print(f"✅ Doc downloaded successfully: {'./'}")
-        return parse_docx_file(response)
+        out = parse_docx_file(file_name)
+        if os.path.exists(file_name):
+            os.remove(file_name)
+        return out
     
     elif "application/vnd.openxmlformats-officedocument.wordprocessingml.document" in content_type:
-        with open('./file.docx', "wb") as file:
+        file_name = f'file_{random.randint(0, 1000)}.docx'
+        with open(file_name, "wb") as file:
             for chunk in response.iter_content(chunk_size=8192):
                 file.write(chunk)
         print(f"✅ Docx downloaded successfully: {'./'}")
-        return parse_docx_file(response)
+        out = parse_docx_file(file_name)
+        if os.path.exists(file_name):
+            os.remove(file_name)
+        return out
     
     else:
         return {"link": link, 
                 "type": "Error: Unsupported content type."}
 
-def main():
+def main(input_path):
     """
     main function
     """
-    print(json.dumps(response, indent=4))
-    if os.path.exists('./file.pdf'):
-        os.remove('./file.pdf')
-    pass
+    if validators.url(input_path) is True:
+        return check_link_content(input_path)
+    if input_path.endswith('.txt'):
+        return parse_text_file(input_path)
+    elif input_path.endswith('.docx'):
+        return parse_docx_file(input_path)
+    elif input_path.endswith('.pdf'):
+        return parse_pdf_file(input_path)
+    elif input_path.endswith('.doc'):
+        return parse_docx_file(input_path)
+    else:
+        return {"link": input_path, 
+                "type": "Error: Unsupported file type."}
     
     
 if __name__=="__main__":
