42cbdc8882be6e3e6b7f64a91716218df80071a1
Sujithrt <sujithramprasad@gmail.com>
2025-03-11 21:32:14 -0700

    moved db credentials to env

--- Backend/database.py
+++ Backend/database.py
@@ -1,5 +1,12 @@
 from databases import Database
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql://postgres:Password@123@localhost/ai-agents-hackathon"
+load_dotenv()
+
+username = os.getenv("POSTGRES_USER")
+password = os.getenv("POSTGRES_PASSWORD")
+database = os.getenv("POSTGRES_DB")
+DATABASE_URL = f"postgresql://{username}:{password}@localhost/{database}"
 
 database = Database(DATABASE_URL)
